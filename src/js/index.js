dff31b7accd6e09a4d723b3a2f3af40cbe44c8d1
Jamal <jamalabdul47@gmail.com>
2019-08-25 10:15:22 -0400

    working on skills section

---
+++ src/js/index.js
@@ -0,0 +1,17 @@
+var $ = require("jquery");
+
+
+$(document).ready(function() {
+  $(window).scroll(function() {
+    if ($(document).scrollTop() > 80) {
+      $('.nav-bar').addClass('shrink');
+      $('.btn-primary').addClass('shrink-btn');
+
+    } else {
+      $('.nav-bar').removeClass('shrink');
+      $('.btn-primary').removeClass('shrink-btn');
+
+    }
+  })
+
+})
